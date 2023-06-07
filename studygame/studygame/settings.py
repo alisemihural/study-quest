7dd9ac2fd431fff7beea5eab0d2b66fe9c8686dc
Ali Semih Ural <60106172+alisemihural@users.noreply.github.com>
2023-06-07 13:37:59 -0400

    Update

--- studygame/studygame/settings.py
+++ studygame/studygame/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2njh*n#2vyqb5_#(i*o9n^=0r7ka4f6!#e!!p2m3rdo8pg^5r-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
